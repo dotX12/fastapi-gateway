56a4ef6c6aa19849139b4cc404557fe1f195b668
dotX12 <hash@shitposting.team>
2021-08-20 15:44:07 +0300

    Added serialize_query_content, unzip_query_params for aiohttp

---
+++ fastapi_gateway/utils/query.py
@@ -0,0 +1,29 @@
+from typing import List, Any, Dict, Optional
+
+from fastapi.routing import serialize_response
+
+
+async def serialize_query_content(key, value) -> dict:
+    serialized_data = await serialize_response(response_content=value)
+    if isinstance(serialized_data, dict):
+        serialized = serialized_data
+    else:
+        serialized = {key: serialized_data}
+    return serialized
+
+
+async def unzip_query_params(
+        all_params: Dict[str, Any],
+        necessary_params: Optional[List[str]] = None,
+) -> Optional[Dict[str, Any]]:
+
+    if necessary_params:
+        response_query_params = {}
+        for key in necessary_params:
+            value = all_params.get(key)
+            serialized_dict = await serialize_query_content(key=key, value=value)
+            response_query_params.update(serialized_dict)
+        return response_query_params
+    return None
+
+
