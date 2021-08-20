3e72c35d5094b32407d144c9867643ae4e8e2685
dotX12 <hash@shitposting.team>
2021-08-20 15:43:10 +0300

    Added unzip_body_object for aiohttp

---
+++ fastapi_gateway/utils/body.py
@@ -0,0 +1,20 @@
+import ujson
+from fastapi.routing import serialize_response
+from typing import Dict, List, Optional, Any
+from aiohttp import JsonPayload
+
+
+async def unzip_body_object(
+        all_params: Dict[str, Any],
+        necessary_params: Optional[List[str]] = None,
+) -> Optional[JsonPayload]:
+
+    if necessary_params:
+        response_body_dict = {}
+        for key in necessary_params:
+            value = all_params.get(key)
+            _body_dict = await serialize_response(response_content=value)
+            response_body_dict.update(_body_dict)
+        return JsonPayload(value=response_body_dict, dumps=ujson.dumps)
+    return None
+
