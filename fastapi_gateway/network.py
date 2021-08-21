84509949d548c3da8fcc6744a4f2ba20c61c4c01
dotX12 <hash@shitposting.team>
2021-08-21 19:43:02 +0300

    Response decoding / Moving business logic.
    - Moved a part of logic on modules.
    - Added a response decoding.

--- fastapi_gateway/network.py
+++ fastapi_gateway/network.py
@@ -1,29 +1,28 @@
-from typing import Optional, Union
-
 import aiohttp
 import async_timeout
+from typing import Optional, Union
 from aiohttp import JsonPayload
-
+from starlette.datastructures import Headers
 from fastapi_gateway.utils.form import CustomFormData
+from fastapi_gateway.utils.response import decode_json
+from fastapi_gateway.utils.request import create_dict_if_not
 
 
 async def make_request(
     url: str,
     method: str,
+    headers: Union[Headers, dict],
     query: Optional[dict] = None,
     data: Union[CustomFormData, JsonPayload] = None,
     timeout: int = 60,
 ):
-
-    if not data:
-        data = {}
-
-    if not query:
-        query = {}
+    data = create_dict_if_not(data=data)
+    query = create_dict_if_not(data=query)
 
     with async_timeout.timeout(timeout=timeout):
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(headers=headers) as session:
             async with session.request(
                     method=method, url=url, params=query, data=data) as response:
-                data = await response.json()
-                return data, response.status, response.headers
+                response_json = await response.json()
+                decoded_json = decode_json(data=response_json)
+                return decoded_json, response.status, response.headers
