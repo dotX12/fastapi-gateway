9ad197adc599c1c010e5dfa8e5b5650bd5245ee8
dotX12 <hash@shitposting.team>
2021-08-21 19:54:39 +0300

    Sending headers from gateway to microservice

--- fastapi_gateway/utils/headers.py
+++ fastapi_gateway/utils/headers.py
@@ -1,4 +1,4 @@
-from starlette.datastructures import MutableHeaders
+from starlette.datastructures import MutableHeaders, Headers
 from typing import Dict, Any
 
 
@@ -6,8 +6,19 @@ def inheritance_service_headers(
         gateway_headers: MutableHeaders,
         service_headers: MutableHeaders,
 ) -> Dict[str, Any]:
-    forced_gateway_headers = ['server', 'date', 'content-encoding', 'content-type']
+    forced_gateway_headers = ['server', 'date', 'content-encoding', 'content-type', 'content-length']
     return {
         key: service_headers[key] for key in service_headers
         if key not in gateway_headers and key.lower() not in forced_gateway_headers
     }
+
+
+def generate_headers_for_microservice(headers: Headers) -> MutableHeaders:
+    new_headers = headers.mutablecopy()
+    gateway_host = headers.get('host')
+    new_headers.append('gateway_host', gateway_host)
+
+    forced_gateway_headers = ['host', 'content-type', 'accept-encoding', 'content-length']
+    [new_headers.__delitem__(key_header) for key_header in forced_gateway_headers]
+
+    return new_headers
